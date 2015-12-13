1ff7dbc95292ebc53cf9caf82b0c395011f5ff93
vicrtor villacis <victor.villacis@outlook.com>
2015-12-12 23:19:00 -0500

    used comma to seperate callback after fast and del an extra fade

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var myWords = ["Rock", "Paper", "Scissors"]; //An array
 
   $("button").click(function() {
-    $(this).fadeOut('slow').fadeIn('fast').fadeIn(function() {
+    $(this).fadeOut('slow').fadeIn('fast', function() {
       var myRandomNumber = Math.floor(Math.random() * myWords.length);
       $("h1").html("<h1>" + myWords[myRandomNumber] + "<br>I am an H1</br>" +"<div><img src='http://dlewis.net/nik-archives/wp-content/uploads/2011/10/Rock-Paper-Scissors.jpg'> </div>");
     });
