aa867ffd3de1966cf226b764c27659eecee52bb9
vicrtor villacis <victor.villacis@outlook.com>
2015-12-12 23:13:30 -0500

    added math random and button animation

--- js/game.js
+++ js/game.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
+  var myWords = ["Rock", "Paper", "Scissors"]; //An array
+
   $("button").click(function() {
-    $(this).fadeOut('fast');
+    $(this).fadeOut('slow').fadeIn('fast').fadeIn(function() {
+      var myRandomNumber = Math.floor(Math.random() * myWords.length);
+      $("h1").html("<h1>" + myWords[myRandomNumber] + "<br>I am an H1</br>" +"<div><img src='http://dlewis.net/nik-archives/wp-content/uploads/2011/10/Rock-Paper-Scissors.jpg'> </div>");
+    });
   });
 });
