d70322777ed4d5c85548796490a07f502b90cf43
vicrtor villacis <victor.villacis@outlook.com>
2015-12-13 02:22:51 -0500

    added score tracking to info buttons

--- js/scoreTracker.js
+++ js/scoreTracker.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-var userScore = 0, computerScore = 0, roundCount =1;
+  var gameState = {
+    userScore: 0,
+    computerScore: 0,
+    roundCount: 1,
+  }
 
-  $("#incUserBtn").click(function(){
-    $("#userScore").html(userScore ++).append("<div>World</div>");
+  $(".btn-info").click(function(){
+    var tagName = $(this).data("tag");
+    gameState[tagName]++;
+    $("#" + tagName).html(gameState[tagName]);
   });
+
 });
