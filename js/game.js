c01be586e66439cb9c5d4c469407349526839796
vicrtor villacis <victor.villacis@outlook.com>
2015-12-15 18:26:19 -0500

    added animation css and javascript/win/lost

--- js/game.js
+++ js/game.js
@@ -2,27 +2,54 @@ $(document).ready(function() {
   var myWords = ["Rock", "Paper", "Scissors"]; //An array
 
   $("#Rock").click(function() {
-    $(this).animate({width:"415px"}).html("<div><img src='http://img07.deviantart.net/a22b/i/2013/053/8/f/the_rock_by_santiagowwe12-d5u3gll.png'></div>").fadeIn('fast', function() {
+    $(this).animate().html("<div class='night-hotel'>Rock</div>").fadeIn('fast', function() {
       var myRandomNumber = Math.floor(Math.random() * myWords.length);
-      $("h1").html("<h1>" + myWords[myRandomNumber] + "<br>I am an H1</br>" +"<div><img src='http://dlewis.net/nik-archives/wp-content/uploads/2011/10/Rock-Paper-Scissors.jpg'> </div>"); 
+      $("h1").html("<h1>" + myWords[myRandomNumber] + "<br>WOW!! YOU WON</br>" +"<div><img src=''> </div>"); 
     });
   });
 
 
 
 $("#Paper").click(function() {
-    $(this).animate({width:"415px"}).html("<div><img src='http://www.ibidworld.com/images/papper%20and%20paper%20product/paper.jpg'></div>").fadeIn('fast', function() {
+    $(this).animate().html("<div class='night-hotel'> Paper</div>").fadeIn('fast', function() {
       var myRandomNumber = Math.floor(Math.random() * myWords.length);
-      $("h1").html("<h1>" + myWords[myRandomNumber] + "<br>I am an H1</br>" +"<div><img src=''> </div>"); 
+      $("h1").html("<h1>" + myWords[myRandomNumber] + "<br>SORRY YOU LOST</br>" +"<div><img src=''> </div>"); 
     });
   });
 
   $("#Scissor").click(function() {
-    $(this).animate({width:"415px"}).html("<div><img src='http://www.macdrifter.com/uploads/2012/08/BigLebowski_Scissors_400px.jpeg'></div>").fadeIn('fast', function() {
+    $(this).animate().html("<div class='night-hotel'>Scissor</div>").fadeIn('fast', function() {
       var myRandomNumber = Math.floor(Math.random() * myWords.length);
-      $("h1").html("<h1>" + myWords[myRandomNumber] + "<br>I am an H1</br>" +"<div><img src=''> </div>"); 
+      $("h1").html("<h1>" + myWords[myRandomNumber] + "<br> WOW!! YOU WON</br>" +"<div><img src=''> </div>"); 
     });
   });
+
+var compare = function (userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return("The result is a tie!");
+    }
+    if (userChoice === "rock") {
+        if (computerChoice === "scissors") {
+            return ("rock wins");
+        } else {
+            return ("paper wins");
+        }
+    }
+    if (userChoice === "paper") {
+        if (computerChoice === "rock") {
+            return ("paper wins");
+        } else {
+            return ("scissors wins");
+        }
+    }
+     if (userChoice=== "scissors") {
+        if (computerChoice === "rock") {
+            return ("rock wins");
+        } else {
+            return ("scissors wins");
+        }
+    }
+};
 
 
 });
