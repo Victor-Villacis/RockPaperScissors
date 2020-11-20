6fce30dc14cf19c6cc3329859b6705312ecdc389
Victor-Villacis <victor.villacis@outlook.com>
2020-11-19 20:12:20 -0500

    adding play style

--- player.js
+++ player.js
@@ -48,64 +48,6 @@ let player1 = new Player();
 let player2 = new Player();
 
 
-// player1.choice = [player1.getRock(), player1.getPaper(),player1.getScissors()];
-// player2.choice = [player2.getRock(), player2.getPaper(),player2.getScissors()];
-
-
-
-
-// function gameLogic2() {
-//   if(rocky1() === 'rock') {
-//     player1 = 'rock';
-//   }
-// }
-
-
-// function rocky1() {
-//   localStorage.rocky1 =  player1.getRock()
-//    player1 = localStorage.rocky1
-//   console.log(player1)
-//   return player1
-// }
-// function paper1() {
-//   player1 =  player1.getPaper()
-//   localStorage.paper1 = player1
-//   console.log(player1)
-//   return player1
-// }
-
-// function scissors1() {
-//   player1 =  player1.getScissors()
-//   localStorage.scissors1 = player1
-//   console.log(player1)
-//   return player1
-// }
-
-// function rocky2() {
-//   player2 =  player2.getRock()
-//   localStorage.rocky2 = player2
-//   console.log(player2)
-//   return player2
-// }
-// function paper2() {
-//   player2 =  player2.getPaper()
-//   localStorage.paper2 = player2
-//   console.log(player2)
-//   return player2
-// }
-
-// function scissors2() {
-//   player2 =  player2.getScissors()
-//   localStorage.scissors2 = player2
-//   console.log(player2)
-//   return player2
-// }
-
-// function test(){
-//    if(player1 === 'rock' && player2 === 'scissors1') {
-//      console.log("I am ok")
-//    }
-// }
 
 
 while(localStorage.content1 === undefined && localStorage.content2 === undefined) {
@@ -122,7 +64,8 @@ if(localStorage.score2 === undefined) {
   localStorage.score2 = Number()
 }
 
- 
+
+
 function gameLogic() {
 
 
@@ -135,115 +78,92 @@ function gameLogic() {
     let a = player1.choice
     let b = player2.choice
 
+   
     
     if(a === "rock" && b === "scissors1") {
       (function () {
         let winner = document.querySelector('#ok')
           winner.innerHTML = localStorage.content1
-          localStorage.removeItem('content1');
-          localStorage.removeItem('content2');
+       
           player1.score = localStorage.score1
           localStorage.score1++
           player1.score = localStorage.score1
           
           console.log(player1.score)
-          setTimeout(function(){
-            location.reload();
-          },3000)
+         
       }());  
-      return localStorage.score1
   }  if(a === "rock" && b === "paper1") {
     (function () {
       let winner = document.querySelector('#ok')
         winner.innerHTML = localStorage.content2
-        localStorage.removeItem('content1');
-        localStorage.removeItem('content2');
+        
+        player2.score = localStorage.score2
         localStorage.score2++
         player2.score = localStorage.score2
-        setTimeout(function(){
-          location.reload();
-        },3000)
+        
+      
     }());  
   }  if(a === "paper" && b === "rock1") {
     (function () {
       let winner = document.querySelector('#ok')
         winner.innerHTML = localStorage.content1
-        localStorage.removeItem('content1');
-        localStorage.removeItem('content2');
-        
-        setTimeout(function(){
-          location.reload();
-        },3000)
+     
+        player1.score = localStorage.score1
+          localStorage.score1++
+          player1.score = localStorage.score1
+      
     }());  
   }  if(a === "paper" && b === "scissors1") {
     (function () {
       let winner = document.querySelector('#ok')
         winner.innerHTML = localStorage.content2
-        localStorage.removeItem('content1');
-        localStorage.removeItem('content2');
-        
-        setTimeout(function(){
-          location.reload();
-        },3000)
+       
+        player2.score = localStorage.score2
+        localStorage.score2++
+        player2.score = localStorage.score2
+      
     }());  
   }  if(a === "scissors" && b === "paper1") {
     (function () {
       let winner = document.querySelector('#ok')
         winner.innerHTML = localStorage.content1
-        localStorage.removeItem('content1');
-        localStorage.removeItem('content2');
+       
+        player1.score = localStorage.score1
+          localStorage.score1++
+          player1.score = localStorage.score1
         
-        setTimeout(function(){
-          location.reload();
-        },3000)
     }());  
   }  if(a === "scissors" && b === "rock1") {
     (function () {
       let winner = document.querySelector('#ok')
         winner.innerHTML = localStorage.content2
-        localStorage.removeItem('content1');
-        localStorage.removeItem('content2');
-        
-        setTimeout(function(){
-          location.reload();
-        },3000)
+     
+        player2.score = localStorage.score2
+        localStorage.score2++
+        player2.score = localStorage.score2
+       
     }());  
-  } if(a === undefined && b === undefined){
-      console.log("you need to make a choice")
-      
+  } if(a === undefined && b === undefined) {
+    let winner = document.querySelector('#ok')
+    winner.innerHTML = `<p>You must make a choice first</p>`
+   
     }
-   else {
-      console.log( `game tied with ${a} " " ${b}`) 
-  }
+   
+
+       if(b === String && a === String) {
+        let winner = document.querySelector('#ok')
+        winner.innerHTML = `<p>I think I am getting there</p>`
+        }
+
+        
 };
 
 
 
-      // console.log( `${localStorage.getItem("Hname1Text")} won with ${localStorage.rocky1}`)
 
-
-// const game = (player1, player2) => {
-//          if(player1 === "rock" && player2 === "scissors") {
-//       return `${localStorage.getItem("Hname1Text")} won with ${player1}`
-//   } else if(player1 === "paper" && player2 === "rock") {
-//       return `${localStorage.getItem("Hname1Text")} won with ${player1}`
-//   } else if(player1 === "scissors" && player2 === "paper") {
-//       return `${localStorage.getItem("Hname1Text")} won with ${player1}`
-//   } else if(player2 === "rock" && player1 === "scissors") {
-//       return `${localStorage.getItem("Hname2Text")} won with ${player2}`
-//   } else if(player2 === "paper" && player1 === "rock") {
-//       return `${localStorage.getItem("Hname2Text")} won with ${player2}`
-//   } else if(player2 === "scissors" && player1 === "paper") {
-//       return `${localStorage.getItem("Hname2Text")} won with ${player2}`
-//   } else {
-//       return "game tied"
-//   }
-// }
-
-
-
-
-
+// setTimeout(function(){
+//   location.reload();
+// },3000)
 
 function subinfo() {
      player1.name = document.getElementById("fname1").value;
@@ -348,6 +268,16 @@ function showPiece(){
       }
 }
 
+function showPiece1(){
+  let addPiece1 = document.querySelector("#target");
+      if(localStorage.content1) {
+        addPiece1.innerHTML = localStorage.content1
+        gameLogic()
+      } else {
+        addPiece1.innerHTML = "<img src='/images/tenor.gif' class='drop-zone'>"
+      }
+}
+
 
 //choose who goes first, then the second player will see the winner. The previous player will only see the winner after he clicks the button. I need to store the winner and 
 //and also keep score and track the score. 
@@ -355,71 +285,6 @@ function showPiece(){
 
 //add a chat? ????????
 /// i will need node then to dynamically have an event loop and call upon the server for informtion. 
-
-// let dragStart = function(e) {
- 
-//     e.dataTransfer.setData('text/plain', e.target.id);
-//     console.log(e.target.id + "lkfsjdlkfjkd")
-  
-// }
-
-// let cancel = function(e) {
-//   if (e.preventDefault) e.preventDefault();
-//   if (e.stopPropagation) e.stopPropagation();
-//   return false;
-// }
-
-// let droppedRock = function(e) {
-
-//     id = e.dataTransfer.getData('text/plain');
-//     e.target.appendChild(document.querySelector("#rock"))
-
-//  }
-
-//  let droppedPaper = function(e) {
-
-//   id = e.dataTransfer.getData('text/plain');
-//   e.target.appendChild(document.querySelector("#paper"))
-  
-
-// }
-
-
-// let droppedScissors = function(e) {
-
-//   id = e.dataTransfer.getData('text/plain');
-//   e.target.appendChild(document.querySelector("#scissors"))
-
-
-// }
-
-
-
-// let rockImg = document.querySelector('#rock');
-// rockImg.addEventListener('dragStart', dragStart, false);
-
-// let paperImg = document.querySelector('#paper');
-// paperImg.addEventListener('dragStart', dragStart, false);
-
-// let scissorsImg = document.querySelector('#scissors');
-// scissorsImg.addEventListener('dragStart', dragStart, false);
-    
-// let targetRock = document.querySelector("#target-container1");
-// let targetPaper = document.querySelector("#target-container2");
-// let targetScissors = document.querySelector("#target-container3");
-    
-//     targetRock.addEventListener('drop', droppedRock, false);
-//     targetPaper.addEventListener('drop', droppedPaper, false);
-//     targetScissors.addEventListener('drop', droppedScissors, false);
-
-    
-//     targetRock.addEventListener('dragenter', cancel, false);
-//     targetPaper.addEventListener('dragenter', cancel, false);
-//     targetScissors.addEventListener('dragenter', cancel, false);
-
-//     targetRock.addEventListener('dragover', cancel, false);
-//     targetPaper.addEventListener('dragover', cancel, false);
-//     targetScissors.addEventListener('dragover', cancel, false);
 
 var cancel = function(e) {
   if (e.preventDefault) e.preventDefault();
@@ -432,16 +297,7 @@ var dropped = function(e) {
 
   var target = this, content = '';
   var types = e.dataTransfer.types;
-  // content = e.dataTransfer.getData(types[2]);
 
-  // var h3 = document.createElement('h3');
-  // h3.innerHTML =    content;
-  // target.appendChild(h3);
- 
-  //   let me = []
-  //   me.push(content)
-
-  //   console.log(me)
 
   if (types.length > 0) {
 
@@ -465,32 +321,16 @@ var dropped = function(e) {
       });
     }
   }
-
-  target.classList.remove('over');
-
-  // If you want to clear all data from 
-  // the dataTransfer object you 
-  // can call:
-  //
-  //  e.dataTransfer.clearData(); 
-  //
-  // or you can clear values based on conent type:
-  //
-  //  e.dataTransfer.clearData('text/plain');
-  //
-  // where 'text/plain' can be 
-  // substituted for any of the 
-  // conent types where you want to 
-  // remove the value.
 };
 
 
-var target = document.getElementById('target1');
-target.addEventListener('drop', dropped, false);
-target.addEventListener('dragenter', cancel, false);
-target.addEventListener('dragover', cancel, false);
-target.addEventListener('dragleave', cancel, false);
-
+var target1 = document.getElementById('target1');
+target1.addEventListener('drop', dropped, false);
+target1.addEventListener('dragenter', cancel, false);
+target1.addEventListener('dragover', cancel, false);
+target1.addEventListener('dragleave', cancel, false);
+target1.addEventListener('gameLogic', dropped, false);
+target1.addEventListener('check', dropped, false);
 
 
 
@@ -505,16 +345,7 @@ var dropped = function(e) {
 
   var target = this, content = '';
   var types = e.dataTransfer.types;
-  // content = e.dataTransfer.getData(types[2]);
 
-  // var h3 = document.createElement('h3');
-  // h3.innerHTML =    content;
-  // target.appendChild(h3);
- 
-  //   let me = []
-  //   me.push(content)
-
-  //   console.log(me)
 
   if (types.length > 0) {
 
@@ -530,56 +361,40 @@ var dropped = function(e) {
             p.innerHTML =  content;
 
         target.appendChild(p);
-        //    let me = []
-        // me.push(content)
-            
-        // console.log(me)
 
       });
     }
   }
-
-  target.classList.remove('over');
-
-  // If you want to clear all data from 
-  // the dataTransfer object you 
-  // can call:
-  //
-  //  e.dataTransfer.clearData(); 
-  //
-  // or you can clear values based on conent type:
-  //
-  //  e.dataTransfer.clearData('text/plain');
-  //
-  // where 'text/plain' can be 
-  // substituted for any of the 
-  // conent types where you want to 
-  // remove the value.
 };
 
+
+function showButton() {
+  showbtn = document.querySelector("#playAgain")
+        showbtn.classList.remove("none")
+}
+
+showButton()
 
 var target = document.getElementById('target');
 target.addEventListener('drop', dropped, false);
 target.addEventListener('dragenter', cancel, false);
 target.addEventListener('dragover', cancel, false);
 target.addEventListener('dragleave', cancel, false);
+target.addEventListener('gameLogic', dropped, false);
+target.addEventListener('check', dropped, false);
 
 
 
 
-var clearButton = document.getElementById('clear');
-clearButton.addEventListener('click', function(e) {
-  e.preventDefault();
-  target.innerHTML = '';
+let playAgain = document.querySelector('#playAgain');
+playAgain.addEventListener('click', function(e) {
+  localStorage.removeItem('content1');
+  localStorage.removeItem('content2');
+  location.reload()
 });
 
+// window.addEventListener('clearButton', function(e) {
+//   location.reload()
+  
+// })
 
-// var xmlString = localStorage.content;
-// var doc = new DOMParser().parseFromString(xmlString, "text/xml");
-// console.log(doc.firstChild.innerHTML); // => <a href="#">Link...
-// console.log(doc.firstChild.firstChild.innerHTML); // => Link
-
-// console.log(localStorage.getItem("content").id)
-// console.log(localStorage.getItem("content"))
-// let condense = localStorage.getItem("content")
-// console.log(condense)
